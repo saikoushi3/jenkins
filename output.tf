4fb83834aae2d0cc10984102ba17e77d12c3c2a0
saikoushi3 <109918157+saikoushi3@users.noreply.github.com>
2025-07-14 12:23:26 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,1 +1,9 @@
+output "jenkins_server_public_ip" {
+  description = "The public IP address of the Jenkins server."
+  value       = aws_instance.jenkins_server.public_ip
+}
 
+output "jenkins_url" {
+  description = "The URL to access Jenkins."
+  value       = "http://${aws_instance.jenkins_server.public_ip}:8080"
+}
